6450713adf1fff8e6bed3eac76f759708150b90f
wangluyao <wangluyao959277@163.com>
2022-01-15 12:05:15 +0800

    feat:初始化项目

---
+++ src/container/Home/index.jsx
@@ -0,0 +1,13 @@
+import Banner from "./component/Banner";
+import Courses from "./component/Courses";
+
+const Home = () => {
+	return (
+		<div>
+			<Banner />
+			<Courses />
+		</div>
+	);
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; // 浏览器性能统计
 
 ReactDOM.render(
+  // 严格模式
   <React.StrictMode>
     <App />
   </React.StrictMode>,
@@ -14,4 +15,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
